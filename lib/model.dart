805cbfb45de81065bdf81bfd1cb1838581ea0b8e
Maria Clara <mcfranca0107@gmail.com>
2022-05-02 15:13:03 -0300

    extensão da classe pessoa para programador

--- lib/model.dart
+++ lib/model.dart
@@ -7,27 +7,38 @@ class Pessoa {
   DateTime? nascimento;
   int? idade;
   Sexo? sexo;
-  double? salario;
-  bool? programador;
 
+  int? getIdade(){
+    if(nascimento == null){
+      return null;
+    }
+    final hoje = DateTime.now();
+    final idade = hoje.difference(nascimento!);
+    return idade.inDays ~/ 365;
+  }
 
   @override
   String toString() {
-    return "nome=$nome, cpf=$cpf, nascimento=$nascimento, idade=$idade, sexo=$sexo, salário=$salario, programador=$programador";
+    return "nome=$nome, cpf=$cpf, nascimento=$nascimento, idade=${getIdade()}, sexo=$sexo";
   }
 }
 
-
+class Programador extends Pessoa{
+  double? salario;
+  @override
+  String toString(){
+    return "nome=$nome, cpf=$cpf, nascimento=$nascimento, idade=${getIdade()}, sexo=$sexo, salário=$salario";
+  }
+}
 
 void mainEntidades(){
-  Pessoa pessoa = Pessoa();
+  Programador pessoa = Programador();
   pessoa.nome = "Reginaldo";
   pessoa.cpf = "111.222.333-44";
   pessoa.nascimento = DateTime(1990, 05, 07);
   pessoa.idade = 31;
   pessoa.sexo = Sexo.Masculino;
-  pessoa.salario = 10000.0;
-  pessoa.programador = true;
+  pessoa.salario = 5000.0;
 
   print(pessoa.toString());
 }
